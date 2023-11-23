cff3a136ea43b317f4a762cd9dd122cee68b90df
Júlio Neves <juliocnevess@gmail.com>
2023-11-23 20:13:57 -0300

    mobile design

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("bundle.js");
-  eleventyConfig.addPassthroughCopy("images/*");
+  eleventyConfig.addPassthroughCopy("images/**/*");
 };
